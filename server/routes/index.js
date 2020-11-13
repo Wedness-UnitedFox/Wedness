b70d1ff4e98ae417913cceabd365bc3d29ea8c28
safruls <safrulsetiawan@icloud.com>
2020-11-14 00:32:49 +0700

    minor user routing revision

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,19 @@
 const routes = require('express').Router() 
-const userController = require("../controllers/userController")
+const userRoutes = require('./userRoutes')
 const vendorRoutes = require('./vendorRoutes')
-const venueRoutes = require('./venueRoutes')
-const organizerRoutes = require('./organizerRoutes')
-const cateringRoutes = require('./cateringRoutes')
-const photoRoutes = require('./photoRoutes')
+
 const errorHandler = require('../middlewares/errorHandler')
 
 routes.get("/", (req,res)=>{
     res.status(200).json({message:"halo home"})
 })
 
-routes.post("/login", userController.userLogin)
-routes.post("/register", userController.userRegister)  
+routes.use("/user", userRoutes)
+
+
 routes.use("/vendor", vendorRoutes)
 
-//auth
-routes.use("/venue", venueRoutes)
-routes.use("/organizer", organizerRoutes)
-routes.use("/catering", cateringRoutes)
-routes.use("/photo", photoRoutes)
+
 routes.use(errorHandler)
 
  
