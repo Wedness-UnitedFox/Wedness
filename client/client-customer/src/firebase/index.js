01b524f43b57991e5616220e7e4b9f5f1fe96a2f
zanadh <fadzim018@gmail.com>
2020-11-16 20:18:23 +0700

    fixing conversation screen

--- client/client-customer/src/firebase/index.js
+++ client/client-customer/src/firebase/index.js
@@ -1,14 +1,15 @@
 
 import firebase from 'firebase'; 
+import 'firebase/firestore'
 
-// const config = {
-//     apiKey: "AIzaSyD6WKr4nBqVaGpVhjBEQ2v1FEDqWuLheko",
-//     authDomain: "letscodescv-5dbc0.firebaseapp.com",
-//     databaseURL: "https://letscodescv-5dbc0.firebaseio.com",
-//     projectId: "letscodescv-5dbc0",
-//     storageBucket: "letscodescv-5dbc0.appspot.com",
-//     messagingSenderId: "313319506594"
-// }
+export const config = {
+    apiKey: "AIzaSyD6WKr4nBqVaGpVhjBEQ2v1FEDqWuLheko",
+    authDomain: "letscodescv-5dbc0.firebaseapp.com",
+    databaseURL: "https://letscodescv-5dbc0.firebaseio.com",
+    projectId: "letscodescv-5dbc0",
+    storageBucket: "letscodescv-5dbc0.appspot.com",
+    messagingSenderId: "313319506594"
+}
 class FirebaseSDK {
     constructor() {
         if (!firebase.apps.length) { //avoid re-initializing
