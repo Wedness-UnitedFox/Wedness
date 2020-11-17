a05a1e997ba96bc89667d38696c7e34a09ac9fd9
zanadh <fadzim018@gmail.com>
2020-11-17 11:13:54 +0700

    edit return customMessage to Not Found

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -20,7 +20,7 @@ class VendorController{
         next({name: 'Wrong Email or Password' })
       }
       else{
-        if(user.role!=='vendor') next({name: 'customMessage', msg:'You cannot use client account on web app' })
+        if(user.role!=='vendor') next({name: 'Not Found'})
         const access_token = signToken({id: user.id, email: user.email, role: user.role})
         res.status(200).json({access_token}) 
       }
