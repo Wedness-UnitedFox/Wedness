f7a5e545698c1f8cd8c2ef56a9bb7e8359b73a30
safruls <safrulsetiawan@icloud.com>
2020-11-17 17:44:04 +0700

    Chat room and dynamic chat convo done

--- client/client-vendor/src/pages/ChatConversation.js
+++ client/client-vendor/src/pages/ChatConversation.js
@@ -1,28 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
 import ChatRoom from './ChatRoom';
-import firebase from '../services/firebase'
+import firebase from '../services/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-const firestore = firebase.firestore
+
+const auth = firebase.auth() 
+const firestore = firebase.firestore()
 
 const ChatConversations = () => {
     const history = useHistory()
     const { path, url } = useRouteMatch()
     // console.log(path, url, "Mantapp")
 
-    // const chatRef = firestore.collection('chats').doc()
-    // console.log(chatRef, "chat Ref");
+    const chatRef = firestore.collection('chats')
+    const query = chatRef.orderBy('createdAt') 
+    const [chats] = useCollectionData(query, {idField: 'id'})
+    const [conversationNames, setConversationNames] = useState([])
+    
+    
+    // useEffect(()=> { //backup
+    //     let conversations = []
+    //     console.log("chats",chats.length;
+    //     if(chats){
+    //         chats.forEach(collection => {
+    //             if(collection.user.customer){
+    //                 conversations.forEach(conversation => {
+    //                     if(conversation !== collection.user.customer){
+    //                         conversations.push(collection.user.customer)
+    //                     }
+    //                 })
+    //             }
+    //         })
+    //         setConversationNames(conversations)
+    //     }
+    // }, [chats])
+
+    useEffect(()=> {
+        console.log(chats?.length,"<<<<<<<<< CHATS");
+        const newChat = chats?.filter(d => d.user.vendor === auth.currentUser.email)
+        let conversations = []
+        console.log("chats",chats?.length);
+        if(chats){
+            newChat.forEach(chat => {   
+                let found = conversations.find(e => e === chat.user.customer)
+                if (!found) {  
+                    conversations.push(chat.user.customer)
+                }
+            })
+            console.log(conversations) 
+            setConversationNames(conversations)
+        }
+    }, [chats])
+
+    // useEffect(()=>{
+    //     console.log(conversationNames, "<<< conv")
+    // },[conversationNames])
     
     return (
-        <>
         <div className="d-flex" style={{width: "400px", height: "100%"}}>
             <div className="border" style={{width: "400px"}}>
                 <h1>Conversations:  </h1>
                 <hr />
-                <ul>
-                    <li><Link to={`${url}/testing@mail.com`}>Chat#1</Link></li>
-                    <li><Link to={`${url}/testing2@mail.com`}>Chat#2</Link></li>
-
+                {/* <Link to={`${url}/testing@mail.com`}>testing@mail.com</Link>
+                <Link to={`${url}/testing2@mail.com`}>testing2@mail.com</Link>
+                 */}
+                <ul> 
+                    {conversationNames?.length===0? null:
+                        conversationNames.map(name=>{
+                           return <li><Link to={`${url}/${name}`}>{name}</Link></li>
+                        }) 
+                    }
                 </ul>
             </div>
             <div className="border">
@@ -31,7 +80,6 @@ const ChatConversations = () => {
                 </Switch>
             </div>
         </div>
-        </>
     )
 }
 
