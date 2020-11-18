f70ec85f03cc95ca792db7e08897d2ec3a65ba4f
safruls <safrulsetiawan@icloud.com>
2020-11-19 03:18:06 +0700

    Invitation Card revised

--- client/client-customer/src/screens/InvitationScreen.js
+++ client/client-customer/src/screens/InvitationScreen.js
@@ -8,6 +8,8 @@ import ViewShot, { captureRef } from 'react-native-view-shot';
 import { View, PermissionsAndroid, Platform, TouchableOpacity, Image, ImageBackground, StyleSheet } from 'react-native';
 import moment from 'moment'
 import TEMPLATE from '../../assets/template_1.jpg'
+import { ScrollView } from 'react-native-gesture-handler';
+import * as Sharing from 'expo-sharing'
 
 const template = Image.resolveAssetSource(TEMPLATE).uri
 
@@ -19,31 +21,56 @@ export default function App({ route, navigation }) {
     const [permission, askForPermission] = Permissions.usePermissions(Permissions.CAMERA_ROLL, { ask: true });
     const viewShotRef = useRef(null)
     const [formattedDate, setFormattedDate] = useState('')
+    const [invitation, setInvitation] = useState(null)
 
     const capture = () => {
         captureRef(viewShotRef, {
             format: "jpg",
             quality: 1
-        }).then(
-            uri => {
-                console.log("Image saved to", uri)
-                MediaLibrary.saveToLibraryAsync(uri)
-                    .then(() => {
-                        alert('sukses')
-                    }).catch(err => {
-                        console.log(err);
-                    })
-
-            },
-            error => console.error("Oops, snapshot failed", error)
-        );
+        })
+        .then( uri => {
+            setInvitation(uri)
+            return MediaLibrary.saveToLibraryAsync(uri)
+        })
+        .then(() => {
+            alert("The invitation has been saved on your phone's gallery")
+        }).catch(err => {
+            console.log(err);
+        })
+        // .catch(error => console.error("Oops, snapshot failed", error))
     }
-    useState(()=>{ 
+
+    const trigger = () => {
+        captureRef(viewShotRef, {
+            format: "jpg",
+            quality: 1
+        })
+        .then( uri => {
+            setInvitation(uri)
+        })
+    }
+
+    useEffect(()=>{ 
         let newDate = new Date(date) 
         newDate =   moment(newDate).format('dddd, Do of MMMM YYYY ')
         console.log(newDate);
         setFormattedDate(newDate)
+        // trigger()
     },[])
+
+    const openShareDialogAsync = async () => {
+        if(!invitation){
+            return alert("Save your invitation first!")
+        }
+        if (!(await Sharing.isAvailableAsync())) {
+          alert(`Uh oh, sharing isn't available on your platform`);
+          return;
+        }
+    
+        await Sharing.shareAsync(invitation.localUri);
+    }; 
+
+
     if (!permission || permission.status !== 'granted') {
         return (
             <View>
@@ -54,7 +81,7 @@ export default function App({ route, navigation }) {
     } else {
         console.log("Granted");
         return (
-            <>
+            <ScrollView>
                 <ViewShot ref={viewShotRef} style={{ width: '100%', height: 600, borderWidth: 1 }} options={{ format: "jpg", quality: 1 }}>
                     <ImageBackground source={{ uri: template }} imageStyle={{ resizeMode: 'contain' }} style={styles.card}>
                         <View style={{ flex: 1 }}>
@@ -71,9 +98,11 @@ export default function App({ route, navigation }) {
                         </View>
                     </ImageBackground>
                 </ViewShot>
-
-                <Button onPress={capture}>Save Image</Button>
-            </>
+                <View style={{flexDirection:"row"}}>
+                    <Button color="#81A68A" onPress={capture} style={{width: "50%"}}>Save Image</Button>
+                    <Button color="#81A68A" onPress={openShareDialogAsync} style={[{width: "50%"}, styles.button]}>Share Image</Button>
+                </View>
+            </ScrollView>
         );
     }
 
@@ -86,7 +115,14 @@ const styles = StyleSheet.create({
         resizeMode: "contain",
         justifyContent: "center",
         alignSelf: "center",
-        backgroundColor: 'grey'
+        // backgroundColor: 'grey'
     },
-
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    button: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
 }) 
